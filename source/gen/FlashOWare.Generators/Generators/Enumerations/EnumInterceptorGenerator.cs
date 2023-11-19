1679c86283785ff840bf4f9dc446093970c9ae3d
Gregor Dostal <10714600+Gh0stWalk3r@users.noreply.github.com>
2023-11-19 12:06:31 +0100

    Apply feedback improvements
    Use named ValueTuple `EnumInvocationGroup`
    Rename variable `group` to `groups`
    Use explicit type instead of var

--- source/gen/FlashOWare.Generators/Generators/Enumerations/EnumInterceptorGenerator.cs
+++ source/gen/FlashOWare.Generators/Generators/Enumerations/EnumInterceptorGenerator.cs
@@ -8,6 +8,8 @@ using FlashOWare.CodeAnalysis;
 using FlashOWare.Text;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
+using EnumInvocationGroup = (string methodName, string enumName);
+
 namespace FlashOWare.Generators.Enumerations;
 
 [Generator(LanguageNames.CSharp)]
@@ -102,17 +104,18 @@ public sealed class EnumInterceptorGenerator : IIncrementalGenerator
 			document.WriteLine('{');
 			document.Indent++;
 
-			IEnumerable<IGrouping<string, EnumMethodInvocation>> group = source.GroupBy(static value => $"{value.Name}:{value.Enum.Name}");
+			IEnumerable<IGrouping<EnumInvocationGroup, EnumMethodInvocation>> groups =
+				source.GroupBy(static value => new EnumInvocationGroup(value.Name, value.Enum.Name));
 
-			foreach (var invocations in group)
+			foreach (IGrouping<EnumInvocationGroup, EnumMethodInvocation> invocations in groups)
 			{
-				var locations = invocations.Select(static invocation => invocation.Location);
-				foreach (var location in locations)
+				IEnumerable<InterceptsLocationInfo> locations = invocations.Select(static invocation => invocation.Location);
+				foreach (InterceptsLocationInfo location in locations)
 				{
 					document.WriteLine($"""[InterceptsLocation(@"{location.FilePath}", {location.Line}, {location.Character})]""");
 				}
 
-				var invocation = invocations.First();
+				EnumMethodInvocation invocation = invocations.First();
 				if (invocation.Name == Methods.GetName)
 				{
 					document.WriteLine($"internal static string? {Methods.GetName}({invocation.Enum.Name} value)");

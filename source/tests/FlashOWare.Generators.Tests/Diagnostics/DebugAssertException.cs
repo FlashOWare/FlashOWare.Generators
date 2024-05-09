7f78a39b010e0365f6be020c9407106b379ca00f
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2024-05-10 01:52:45 +0200

    test: add Debug-TraceListener for .NET Framework

---
+++ source/tests/FlashOWare.Generators.Tests/Diagnostics/DebugAssertException.cs
@@ -0,0 +1,20 @@
+#if DEBUG && NETFRAMEWORK
+using System.Diagnostics.CodeAnalysis;
+
+namespace FlashOWare.Tests.Diagnostics;
+
+[SuppressMessage("Design", "CA1032:Implement standard exception constructors", Justification = "DEBUG && NETFRAMEWORK")]
+[SuppressMessage("Design", "CA1064:Exceptions should be public", Justification = "DEBUG && NETFRAMEWORK")]
+internal sealed class DebugAssertException : Exception
+{
+	public DebugAssertException(string? message)
+		: base(message)
+	{
+	}
+
+	public DebugAssertException(string? message, string? detailMessage)
+		: base($"{message}{Environment.NewLine}{detailMessage}")
+	{
+	}
+}
+#endif
